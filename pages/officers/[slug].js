63586804e53e74dc3722471909ab439e485fac71
IHIutch <jbhutch01@gmail.com>
2021-02-20 11:51:13 -0500

    Connect officer slug, update officer page

--- pages/officers/[slug].js
+++ pages/officers/[slug].js
@@ -1,7 +1,8 @@
 import Head from "next/head";
-import supabase from "../util/supabase";
+import supabase from "@/util/supabase";
 import { useTable, useSortBy } from "react-table";
-import Container from "../components/common/container";
+import Container from "@/components/common/container";
+import NextLink from "next/link";
 import {
   Box,
   Grid,
@@ -16,56 +17,77 @@ import {
   Th,
   Thead,
   Tr,
+  Wrap,
+  WrapItem,
 } from "@chakra-ui/react";
 import { useMemo } from "react";
-import NextLink from "next/link";
+import dayjs from "dayjs";
 
-const Home = ({ allegations }) => {
+const Officer = ({ officer }) => {
   const columns = useMemo(() => [
     {
-      Header: "Name",
-      accessor: (originalRow) => ({
-        name: `${originalRow.first_name} ${originalRow.last_name}`,
-        slug: originalRow.slug,
-      }),
+      Header: "Open Date",
+      accessor: "open_date",
       Cell: ({ value }) => (
-        <NextLink href={`/officers/${value.slug}`} passHref>
-          <Tag as={Link}>{value.name}</Tag>
-        </NextLink>
+        <Box as="span" whiteSpace="nowrap">
+          {value ? dayjs(value).format("MMM. DD, YYYY") : "Unknown"}
+        </Box>
       ),
     },
     {
-      Header: "Rank",
-      accessor: "rank",
-    },
-    {
-      Header: "Unit",
-      accessor: "unit",
-    },
-    {
-      Header: "Allegations",
-      accessor: "allegations_count",
+      Header: "Complaints",
+      accessor: "complaints",
       Cell: ({ value }) => (
-        <Text as="span" fontWeight="bold">
-          {value}
-        </Text>
+        <Wrap>
+          {value &&
+            value.map((v, idx) => (
+              <WrapItem key={idx}>
+                <NextLink
+                  href={`/complaints/${v.complaint_type.slug}`}
+                  passHref
+                >
+                  <Tag as={Link}>{v.complaint_type.name}</Tag>
+                </NextLink>
+              </WrapItem>
+            ))}
+        </Wrap>
+      ),
+    },
+    {
+      Header: "Disposition Date",
+      accessor: "disposition_date",
+      Cell: ({ value }) => (
+        <Box as="span" whiteSpace="nowrap">
+          {value ? dayjs(value).format("MMM. DD, YYYY") : "Unknown"}
+        </Box>
+      ),
+    },
+    {
+      Header: "Dispositions",
+      accessor: "dispositions",
+      Cell: ({ value }) => (
+        <Wrap spacing="2">
+          {value &&
+            value.map((v, idx) => (
+              <WrapItem key={idx}>
+                <NextLink
+                  href={`/dispositions/${v.disposition_type.slug}`}
+                  passHref
+                >
+                  <Tag as={Link}>{v.disposition_type.name}</Tag>
+                </NextLink>
+              </WrapItem>
+            ))}
+        </Wrap>
       ),
     },
   ]);
-
-  const formattedData = allegations.map((a) => {
-    return {
-      ...a,
-      allegations_count: a.allegations.length,
-    };
-  });
-
-  const data = useMemo(() => formattedData, formattedData);
+  const data = useMemo(() => officer.allegations, officer.allegations);
 
   return (
     <div>
       <Head>
-        <title>Buffalo Watchdog</title>
+        <title>{`${officer.first_name} ${officer.last_name}`}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container>
@@ -81,7 +103,7 @@ const Home = ({ allegations }) => {
                   letterSpacing="0.125rem"
                   lineHeight="0.5rem"
                 >
-                  Overview
+                  Officer
                 </Text>
                 <Heading
                   as="h1"
@@ -90,7 +112,7 @@ const Home = ({ allegations }) => {
                   letterSpacing="-0.1rem"
                   mb="4"
                 >
-                  Buffalo Watchdog
+                  {`${officer.first_name} ${officer.last_name}`}
                 </Heading>
                 <Text color="gray.700" fontSize="2xl">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
@@ -120,8 +142,8 @@ const DataTable = ({ columns, data }) => {
       initialState: {
         sortBy: [
           {
-            id: "allegations_count",
-            desc: true,
+            id: "open_date",
+            desc: false,
           },
         ],
       },
@@ -165,10 +187,27 @@ const DataTable = ({ columns, data }) => {
   );
 };
 
-export async function getStaticProps() {
+export async function getStaticPaths() {
+  const { data } = await supabase.from("officers").select("*");
+
+  const paths = data.map((o) => ({
+    params: { slug: o.slug },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
   const { data, error } = await supabase
     .from("officers")
-    .select("*, allegations(*)");
+    .select(
+      `*, 
+      allegations(*, 
+        complaints: allegation_to_complaint(*, complaint_type: complaint_types(*)),
+        dispositions: allegation_to_disposition(*, disposition_type: disposition_types(*)))`
+    )
+    .eq("slug", params.slug)
+    .single();
 
   if (error) {
     return {
@@ -178,9 +217,9 @@ export async function getStaticProps() {
 
   return {
     props: {
-      allegations: data,
+      officer: data,
     },
   };
 }
 
-export default Home;
+export default Officer;
