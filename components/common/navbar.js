b443aa47634c3e2e9289ad1ae0a6642c0d56e398
IHIutch <jbhutch01@gmail.com>
2021-02-21 10:57:45 -0500

    Update nav links

--- components/common/navbar.js
+++ components/common/navbar.js
@@ -22,10 +22,10 @@ const Navbar = ({ sx }) => {
   };
 
   const menuItems = [
-    {
-      name: "Officers",
-      path: "/dashboard",
-    },
+    // {
+    //   name: "Officers",
+    //   path: "/officers",
+    // },
     {
       name: "Allegations",
       path: "/allegations",
@@ -41,78 +41,78 @@ const Navbar = ({ sx }) => {
   ];
 
   return (
-    <Box
-      style={{ backdropFilter: "blur(8px)" }}
-      as="nav"
-      bg="whiteAlpha.800"
-      shadow="sm"
-      position="fixed"
-      top="0"
-      left="0"
-      right="0"
-      sx={sx}
-    >
-      <Container>
-        <Flex wrap="wrap" align="center">
-          <Box mr="12">
-            <NextLink href="/" passHref>
-              <Link
-                d="flex"
-                alignItems="center"
-                px="4"
-                mx="-4"
-                h="16"
-                _hover=""
-              >
-                <Heading as="h1" size="md">
-                  Buffalo Watchdog
-                </Heading>
-              </Link>
-            </NextLink>
-          </Box>
-
-          <CloseButton
-            ml="auto"
-            d="flex"
-            d={{ base: "block", md: "none" }}
-            onClick={onToggle}
-          >
-            <Icon as={isOpen ? X : Menu} h="6" w="6" />
-          </CloseButton>
-
-          <Box
-            alignItems="stretch"
-            h="100%"
-            d={{ base: isOpen ? "block" : "none", md: "flex" }}
-            w={{ base: "full", md: "auto" }}
-          >
-            {menuItems.map((link, idx) => (
-              <NextLink key={idx} href={link.path} passHref>
+    <Box mt="16">
+      <Box
+        style={{ backdropFilter: "blur(8px)" }}
+        as="nav"
+        bg="whiteAlpha.800"
+        shadow="sm"
+        position="fixed"
+        top="0"
+        left="0"
+        right="0"
+        sx={sx}
+      >
+        <Container>
+          <Flex wrap="wrap" align="center">
+            <Box mr="12">
+              <NextLink href="/" passHref>
                 <Link
-                  h="16"
-                  d={{ base: "flex", md: "inline-flex" }}
-                  sx={
-                    isPathMatch(link.path)
-                      ? {
-                          bg: { base: "black", md: "transparent" },
-                          boxShadow: { md: "inset 0 -3px black" },
-                          color: { base: "white", md: "black" },
-                        }
-                      : { color: "gray.500" }
-                  }
-                  _hover={{ color: { md: "black" } }}
-                  rounded={{ base: "md", md: "none" }}
-                  fontWeight="semibold"
+                  d="flex"
                   alignItems="center"
                   px="4"
+                  mx="-4"
+                  h="16"
+                  _hover=""
                 >
-                  {link.name}
+                  <Heading as="h1" size="md">
+                    Buffalo Watchdog
+                  </Heading>
                 </Link>
               </NextLink>
-            ))}
-          </Box>
-        </Flex>
-      </Container>
+            </Box>
+            <CloseButton
+              ml="auto"
+              d="flex"
+              d={{ base: "block", md: "none" }}
+              onClick={onToggle}
+            >
+              <Icon as={isOpen ? X : Menu} h="6" w="6" />
+            </CloseButton>
+            <Box
+              alignItems="stretch"
+              h="100%"
+              d={{ base: isOpen ? "block" : "none", md: "flex" }}
+              w={{ base: "full", md: "auto" }}
+            >
+              {menuItems.map((link, idx) => (
+                <NextLink key={idx} href={link.path} passHref>
+                  <Link
+                    h="16"
+                    d={{ base: "flex", md: "inline-flex" }}
+                    sx={
+                      isPathMatch(link.path)
+                        ? {
+                            bg: { base: "black", md: "transparent" },
+                            boxShadow: { md: "inset 0 -3px black" },
+                            color: { base: "white", md: "black" },
+                          }
+                        : { color: "gray.500" }
+                    }
+                    _hover={{ color: { md: "black" } }}
+                    rounded={{ base: "md", md: "none" }}
+                    fontWeight="semibold"
+                    alignItems="center"
+                    px="4"
+                  >
+                    {link.name}
+                  </Link>
+                </NextLink>
+              ))}
+            </Box>
+          </Flex>
+        </Container>
+      </Box>
     </Box>
   );
 };
