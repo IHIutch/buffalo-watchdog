7517cc5f110595e0bf5e529cf88b9b2f5511bd4f
IHIutch <jbhutch01@gmail.com>
2021-02-23 14:23:50 -0500

    Add plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import PlausibleProvider from "next-plausible";
 
 const MyApp = ({ Component, pageProps }) => (
-  <ChakraProvider>
-    <Component {...pageProps} />
-  </ChakraProvider>
+  <PlausibleProvider domain="buffalo-watchdog.vercel.app/">
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  </PlausibleProvider>
 );
 
 export default MyApp;
