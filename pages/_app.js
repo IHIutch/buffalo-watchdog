418f02c26d501eb611355c0fd3a8269e51c3845d
JB Hutch <jbhutch01@gmail.com>
2022-04-25 08:48:47 -0400

    Add fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,28 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import PlausibleProvider from "next-plausible";
+import { ChakraProvider } from '@chakra-ui/react'
+import PlausibleProvider from 'next-plausible'
 
-const MyApp = ({ Component, pageProps }) => (
-  <PlausibleProvider domain="buffalo-watchdog.vercel.app">
+export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      Fathom.load('WUGKAEDF')
+
+      function onRouteChangeComplete() {
+        Fathom.trackPageview()
+      }
+      // Record a pageview when route changes
+      router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+      // Unassign event listener
+      return () => {
+        router.events.off('routeChangeComplete', onRouteChangeComplete)
+      }
+    }
+  }, [router.events])
+  return (
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
-  </PlausibleProvider>
-);
-
-export default MyApp;
+  )
+}
